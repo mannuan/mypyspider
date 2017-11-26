39c7716ad5a56b2c464f511cd056b29c2d49852a
mannuan <mannuan@mannuan.mannuan>
2017-11-26 23:45:09 +0800

    环保部

--- other_crawl/website.sql
+++ other_crawl/website.sql
@@ -14,11 +14,12 @@ CREATE TABLE `website` (
   `type_id` int(5) DEFAULT '0',
   `file_url` varchar(500) DEFAULT NULL,
   `file_name` varchar(100) DEFAULT NULL,
-  `source` varchar(100) NOT NULL DEFAULT '浙江水利厅',
+  `source` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-#添加url唯一索引
+
+-- 添加url唯一索引
 -- alter table website modify column id int(10) NOT NULL;
 -- alter table website drop primary key;
 -- alter table website add primary key(id);
@@ -26,4 +27,4 @@ CREATE TABLE `website` (
 --
 -- SET SQL_SAFE_UPDATES = 0;
 
-ALTER TABLE `note` DEFAULT CHARACTER SET utf8mb4;
+-- ALTER TABLE `note` DEFAULT CHARACTER SET utf8mb4;
