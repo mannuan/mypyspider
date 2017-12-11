c9a80f90168a1ef2084439ce644c771c39870e43
mannuan <1271990125@qq.com>
2017-12-11 15:22:47 +0800

    dazhongdianping_finish

--- dazhongdianping/shop_spider2.py
+++ dazhongdianping/shop_spider2.py
@@ -4,7 +4,7 @@
 # Project: sdad
 
 from pyspider.libs.base_handler import *
-import json,pymysql
+import json,pymysql,time
 
 
 class Handler(BaseHandler):
@@ -21,15 +21,14 @@ class Handler(BaseHandler):
         cityid=3602
         for i in range(120):
             start = i*limit
-            url = 'https://m.dianping.com/isoapi/module'
+            url = 'http://m.dianping.com/isoapi/module'
             url += "?start={}&cityid={}".format(start,cityid)
             data = {"moduleInfoList":[{"moduleName":"mapiSearch","query":{"search":{"start":start,"limit":limit,"cityid":cityid},"loaders":["list"]}}],"pageEnName":"shopList"}
             data = json.dumps(data)
-            self.crawl(url, method='POST', data=data, callback=self.callback)
+            self.crawl(url, method='POST', data=data, callback=self.index_page)
 
     @config(age=10 * 24 * 60 * 60)
-    def callback(self, response):
-        result = []
+    def index_page(self, response):
         ob_json = response.json
         list_shops = ob_json.get('data').get('moduleInfoList')[0].get('moduleData').get('data')
         if list_shops is None:
@@ -40,42 +39,23 @@ class Handler(BaseHandler):
             else:
                 list_shops = list_shops.get('listData').get('list')
         for shop in list_shops:  # 遍历
-            if (shop.get('adShop') is True):
-                adInfo = json.dumps(shop.get('adInfo'))  # 广告信息,一个json字符串
-            else:
-                adInfo = None
-            altName = shop.get('altName')
             authorityLabelType = shop.get('authorityLabelType')  # int
-            if (shop.get('bookable') is True):
-                bookType = shop.get('bookType')  # str
-            else:
-                bookType = None
             branchName = shop.get('branchName')  # 店铺所属分支
             categoryId = shop.get('categoryId')  # 菜系id,int
             categoryName = shop.get('categoryName')  # 菜系
             cityId = shop.get('cityId')  # 所在城市的id,int
             defaultPic = shop.get('defaultPic')  # 店铺的头像
             dishtags = shop.get('dishtags')  # 菜的种类
-            extraJson = shop.get('extraJson')  # str
-            if (shop.get('hasDeals') is True):
-                shopDealInfos = json.dumps(shop.get('shopDealInfos'))
-            else:
-                shopDealInfos = None
             id = shop.get('id')  # 店铺的id,int
             matchText = shop.get('matchText')  # 店铺的匹配字段
-            memberCardId = shop.get('memberCardId')  # int
             name = shop.get('name')  # 店铺的名字
             newShop = str(shop.get('newShop'))  # 布尔类型
             orderDish = str(shop.get('orderDish'))  # 布尔类型
-            originalUrlKey = shop.get('originalUrlKey')  # 店铺的头像(原本的大小)
             priceText = shop.get('priceText')  # 店铺的平均价格
-            recommendReasonData = json.dumps(shop.get('recommendReasonData'))
             regionName = shop.get('regionName')  # 店铺所属的行政区
             reviewCount = shop.get('reviewCount')  # 店铺的评论数,int
             scoreText = shop.get('scoreText')
-            shopPositionInfo = json.dumps(shop.get('shopPositionInfo'))
             shopPower = shop.get('shopPower')  # 店铺的评分(总分50),int
-            shopStateInformation = json.dumps(shop.get('shopStateInformation'))
             shopType = shop.get('shopType')  # 店铺的类别,int
             status = shop.get('status')  # 店铺的状态,int
             tagList = shop.get('tagList')
@@ -85,10 +65,22 @@ class Handler(BaseHandler):
                 tag_list = list()
                 for tag in tagList:
                     tag_list.append(tag.get('text'))
-                tag = json.dumps(tag_list)
+                tag = ''
+                for tl in tag_list:
+                    tag += tl+','
 
-            result.append([adInfo,altName,authorityLabelType,bookType,branchName,categoryId,categoryName,cityId,defaultPic,dishtags,extraJson,shopDealInfos,id,matchText,memberCardId,name,newShop,orderDish,originalUrlKey,priceText,recommendReasonData,regionName,reviewCount,scoreText,shopPositionInfo,shopPower,shopStateInformation,shopType,status,tag])
-        # print result
+            result = []
+            result.extend([authorityLabelType,branchName,categoryId,categoryName,cityId,defaultPic,dishtags,id,matchText,name,newShop,orderDish,priceText,regionName,reviewCount,scoreText,shopPower,shopType,status,tag])
+            # print id
+            self.crawl('http://m.dianping.com/shop/{}/map'.format(id), fetch_type='js', save={'result':result}, callback=self.detail_page)
+
+    @config(priority=2)
+    def detail_page(self, response):
+        obj = response.text.split('window.PAGE_INITIAL_STATE = ')[1].split(';\n    </script>')[0]
+        address = json.loads(obj).get('_context').get('pageInitData').get('address')
+        crawl_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))  # 爬虫的时间
+        result = [address,crawl_time]
+        result.extend(response.save['result'])
         return result
 
     def on_result(self, result):
@@ -97,9 +89,9 @@ class Handler(BaseHandler):
         conn = pymysql.connect(host='127.0.0.1', port=3306, user='repository', passwd='repository', db='repository',charset='utf8mb4')
         cur = conn.cursor()
         try:
-            sql = 'REPLACE INTO dazhongdianping_meishi_shop values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            sql = 'REPLACE INTO dazhongdianping_shop values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
             # 批量插入
-            cur.executemany(sql,result)
+            cur.execute(sql,result)
             conn.commit()
         except Exception as e:
             print e
